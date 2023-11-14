c32e1c15bc4529adab2eea641f87aaac902585f2
SimulatedDeer <distracteddeer@gmail.com>
2023-11-13 18:37:21 -0700

    Just Svelte/Skelly Init - ignore no valid run files yet

--- frontend/BizzFuzz/src/routes/+page.svelte
+++ frontend/BizzFuzz/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+
+	</div>
+</div>
