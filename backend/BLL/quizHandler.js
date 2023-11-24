7f99268163ef45641b4e2c4978255b482509141c
Breeves03 <126525979+Breeves03@users.noreply.github.com>
2023-11-24 15:54:19 -0700

    added quizhandler

---
+++ backend/BLL/quizHandler.js
@@ -0,0 +1,24 @@
+
+const data = require('../data/data.js');
+
+
+/**
+ * takes the user Id and returns 9 new quizes in an array
+ * 
+ * @param {String} UserId the id of the user object that is to be deleted
+ * @param {function(result,err)} callback passed function to handle the result and error outcome (result = array of quizzes)
+ */
+const getNewQuizes = (userId, callback) =>{
+
+
+
+}
+
+
+
+
+module.exports = {
+    getNewQuizes: getNewQuizes
+ }
+ 
+ 
