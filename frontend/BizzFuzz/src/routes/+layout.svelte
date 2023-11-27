a54d4fe91331e48130aa1ec5867dfe27e7534bcf
SimulatedDeer <distracteddeer@gmail.com>
2023-11-27 00:25:22 -0700

    Scrapped Svelte project from pastify in order to ensure project can be made on time. Getting the site up took so much more time than I thought

---
+++ frontend/BizzFuzz/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.postcss";
+  import "..//styles.css";
+  import Header from "$lib/Navigation/MainHeader.svelte";
+</script>
+
+
+<div class="app">
+
+
+  <main>
+    <slot />
+  </main>
+
+</div>
+
---
+++ frontend/BizzFuzz/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { onDestroy, onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+
+
+
+	onMount(() => {
+        goto('/home');
+    });
+
+
+</script>
+
+
+<svelte:head>
+	<title>Pastify</title>
+	<meta name="Pastify" content="Pastify" />
+</svelte:head>
