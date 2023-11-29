7866ef491a2b34d4cd0665312c89d6a8e6391aa5
callanjerel <333221.rob@gmail.com>
2023-11-28 17:25:17 -0700

    Small changes to validation and docs
    Now /quizlets/ GET requires a page field

--- backend/validation/quizletValidation.js
+++ backend/validation/quizletValidation.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
 
-const userIdSchema = Joi.object({
-    userId: Joi.string().required()
+const quizletRequestSchema = Joi.object({
+    userId: Joi.string().required(),
+    page: Joi.number().integer().min(1)
 })
 const validateQuizletGet = (req, res, next) => {
-    const { error } = userIdSchema.validate(req.body);
+    const { error } = quizletRequestSchema.validate(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
