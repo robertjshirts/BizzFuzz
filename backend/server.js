aac7d043654804a82ad9f0d4c1698068f25d6610
Robert Shirts <75270197+robertjshirts@users.noreply.github.com>
2023-12-06 06:49:08 -0700

    Changed server to 3001, CORS to 3000

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const session = require('express-session');
 const cors = require('cors');
 const path = require('path');
@@ -14,7 +14,7 @@ app.use(session({
     cookie: { secure: false } 
 }));
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 
