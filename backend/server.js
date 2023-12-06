510536824021b10c2d5ed3395be737ac9dffe585
Robert Shirts <75270197+robertjshirts@users.noreply.github.com>
2023-12-06 06:49:33 -0700

    Made sure port is always 3001

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 const session = require('express-session');
 const cors = require('cors');
 const path = require('path');
